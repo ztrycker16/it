25db6c2b5360229f460cc86275e9b9d3a2234c2a
ztrycker16 <50925951+ztrycker16@users.noreply.github.com>
2019-05-22 14:43:36 -0500

    Update head.php

--- template/head.php
+++ template/head.php
@@ -16,7 +16,7 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
-	<link rel="icon" type="image/png" href="<?php echo $itiz->img?>/itiz.png">
+	<link rel="icon" type="image/png" href="assets/img/itiz.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>Instituto Tecnológico de Iztapalapa</title>
 
